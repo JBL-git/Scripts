91e2edbe1ab47454082114c1ccf5d9026eb2fe16
JBL-git <222495480+JBL-git@users.noreply.github.com>
2025-07-26 21:26:13 -0400

    Update auto_arch.sh

--- install-scripts/auto_arch.sh
+++ install-scripts/auto_arch.sh
@@ -1,110 +1,91 @@
 #!/bin/bash
-
 set -e
 
-# sets time
+echo "Setting up NTP..."
 timedatectl set-ntp true
 
-echo "partitioning disk with fdisk..."
-
-fdisk /dev/vda << EOF
-g     # create new GPT partition table
-n     # new parition 1 (EFI)
-1     # partition 1
-      # default start
+echo "Partitioning /dev/vda..."
+fdisk /dev/vda <<EOF
+g       
+n       
+1
+        
 +512M
-t     # change type
+t       
 1
 1
-n     # new partition 2 (root)
-2     # parition number 2
-      # default start
-      # default end
-w     # write changes
+n      
+2
+        
+        
+w       
 EOF
 
-echo "formating partitions..."
-
-mkfs.fat -32 /dev/vda1
+echo "Formatting partitions..."
+mkfs.fat -F32 /dev/vda1
 mkfs.ext4 /dev/vda2
 
-echo "mounting paritions..."
-
+echo "Mounting partitions..."
 mount /dev/vda2 /mnt
-mkdir /mnt/boot
+mkdir -p /mnt/boot
 mount /dev/vda1 /mnt/boot
 
-echo "installing base system..."
+echo "Installing base system..."
+pacstrap /mnt base linux linux-firmware nano vim networkmanager sudo grub efibootmgr git base-devel
 
-pacstrap /mnt base linux linux-firmware nano vim networkmanager sudo grub efibootmgr
-
-echo "generating fstab..."
-
+echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "chrooting..."
+echo "Entering chroot to configure system..."
 
-arch-chroot /mnt /bin/bash
-echo "setting the timezone..."
+arch-chroot /mnt /bin/bash <<'EOF'
+set -e
+
+echo "Generating and setting root password..."
+rootpass=$(openssl rand -base64 16)
+echo "root:$rootpass" | chpasswd
+echo "$rootpass" > /root/root_password.txt
+chmod 600 /root/root_password.txt
+echo "Root password saved to /root/root_password.txt"
+
+echo "Creating user 'archuser' and setting password..."
+username="archuser"
+userpass=$(openssl rand -base64 16)
+useradd -m -G wheel -s /bin/bash "$username"
+echo "$username:$userpass" | chpasswd
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+echo "$username:$userpass" > /root/user_password.txt
+chmod 600 /root/user_password.txt
+echo "User password saved to /root/user_password.txt"
+
+echo "Setting timezone to UTC..."
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-
-echo "setting the hardware clock..."
-
 hwclock --systohc
 
-echo "setting locale..."
-
+echo "Configuring locale..."
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-
-cat <<EOF > /etc/locale.conf
-LANG=en_US.UTF-8
-EOF
-
-echo "setting language..."
-
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-echo "setting the hostname..."
-
+echo "Setting hostname..."
 echo "arch" > /etc/hostname
 
-cat <<EOT > /etc/hosts
+echo "Configuring hosts file..."
+cat > /etc/hosts <<EOL
 127.0.0.1    localhost
 ::1          localhost
 127.0.1.1    arch.localdomain arch
-EOT
+EOL
 
-# installing and configuring the grub bootloader
-
-echo "getting GRUB ready..."
-
+echo "Installing and configuring GRUB bootloader..."
 mount /dev/vda1 /boot
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "setting root password and creating a user..."
-
-rootpass=$(openssl rand -base64 16)
-echo "root:$rootpass" | chpasswd
-echo "saving the password to a file /root/root_password.txt"
-echo "$rootpass" > /root/root_password.txt
-chmod 600 /root/root_password.txt
-echo "Root password: $rootpass"
-
-username="archuser"
-userpass=\$(openssl rand -base64 16)
-useradd -m -G wheel -s /bin/bash \$username
-echo "\$username:\$userpass" | chpasswd
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-echo "User '\$username' password: \$userpass" > /root/user_password.txt
-chmod 600 /root/user_password.txt
-
-
-echo "installing parallel downloads in pacman..."
+echo "Enabling parallel downloads in pacman..."
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
-echo "installing hyprland and other packages..."
+echo "Installing Hyprland and essential packages..."
 pacman -Sy --noconfirm \
   hyprland \
   xdg-desktop-portal-hyprland \
@@ -126,57 +107,47 @@ pacman -Sy --noconfirm \
   unrar \
   pavucontrol \
   neovim \
-  git
+  git \
+  sddm \
+  mpv \
+  ranger \
+  ufw
 
-echo "enabling network access on boot..."
-systemctl enable NetworkManager
+echo "Enabling systemd services..."
+systemctl enable NetworkManager sddm ufw
 
+echo "Configuring UFW firewall..."
+ufw default deny incoming
+ufw default allow outgoing
+echo "y" | ufw enable
 
-username="archuser"
-
-mkdir -p /home/\$username/.config/hypr
-cp -r /etc/xdg/hypr/* /home/\$username/.config/hypr/
-chown -R \$username:\$username /home/\$username/.config/hypr
-
-mkdir -p /etc/systemd/system/getty@tty1.service.d/
-cat <<EOT > /etc/systemd/system/getty@tty1.service.d/override.conf
-[Service]
-ExecStart=
-ExecStart=-/usr/bin/agetty --autologin archuser --noclear %I \$TERM
-EOT
-EOF
-
-username="archuser"  
-
-echo '[[ -z \$DISPLAY && \$XDG_SESSION_TYPE != "wayland" ]] && exec Hyprland' >> /home/\$username/.bash_profile
-chown \$username:\$username /home/\$username/.bash_profile
-
-echo "[*] installing sddm, Brave, Ranger, mpv, and ufw..."
-
-# enable multilib repo and add AUR helper (yay) for Brave
+echo "Setting up AUR helper yay for Brave Browser installation..."
 sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
 pacman -Sy --noconfirm
 
-pacman -S --noconfirm sddm mpv ranger ufw git base-devel
-
-# enable sddm service
-systemctl enable sddm
-
-# install yay (AUR helper) to install brave browser
-git clone https://aur.archlinux.org/yay.git /tmp/yay
-cd /tmp/yay
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si --noconfirm
 
-# install brave browser
+echo "Installing Brave browser..."
 yay -S --noconfirm brave-bin
 
-# enable and configure UFW
-systemctl enable ufw
-ufw default deny incoming
-ufw default allow outgoing
-ufw enable
+echo "Setting up autologin on tty1 for user $username..."
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
+cat > /etc/systemd/system/getty@tty1.service.d/override.conf <<EOL
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin $username --noclear %I \$TERM
+EOL
+
+echo "Setting up Hyprland autostart in user's bash_profile..."
+echo '[[ -z $DISPLAY && $XDG_SESSION_TYPE != "wayland" ]] && exec Hyprland' >> /home/$username/.bash_profile
+chown $username:$username /home/$username/.bash_profile
+
 EOF
 
-echo "exiting chroot..."
+echo "Unmounting partitions..."
 umount -R /mnt
-reboot
+
+echo "Installation complete. Please reboot..."
